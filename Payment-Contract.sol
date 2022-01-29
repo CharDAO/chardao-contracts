f7f23c71087147650a4c12c210ed8e75395755c9
Nicholas White <nkwkona@gmail.com>
2022-01-28 22:02:37 -0500

    test

--- Payment-Contract.sol
+++ Payment-Contract.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >= 0.7.0 <0.9.0;
 contract Donate{
-    //This is the payout structure
+    //This is the payout structure g
 
     uint userIdNumber = 0;
     address public broker;
